cb80b5b4d9ea8e88169815ac1451f1c59c3263be
Badsha-Nishan <badshanisan14@gmail.com>
2026-03-04 17:48:49 +0600

    implement card display section

--- index.js
+++ index.js
@@ -5,6 +5,37 @@ const loadLessons = () => {
     .then((data) => displayLessons(data.data));
 };
 
+const loadLevelWord = (id) => {
+  fetch(`https://openapi.programming-hero.com/api/level/${id}`)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
+};
+
+const displayLevelWord = (words) => {
+  // 1. Get the Container and empty
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
+  //   2. Get into every word
+  for (const word of words) {
+    // 3.Create element
+    const card = document.createElement("div");
+    card.innerHTML = `
+        <div class="bg-white space-y-4 rounded py-8">
+          <h2 class="text-3xl font-bold">${word.word}</h2>
+          <p class="text-xl font-medium"> Meaning / Pronunciation</p>
+          <h2 class="font-bangla font-semibold text-3xl">"${word.meaning} / ${word.pronunciation}"</h2>
+          <div class="flex justify-between px-12">
+            <button class="btn bg-[#1A91FF20]">
+              <i class="fa-solid fa-circle-info"></i>
+            </button>
+            <button class="btn bg-[#1A91FF20]"><i class="fa-solid fa-volume-high"></i></button>
+          </div>
+        </div>
+    `;
+    wordContainer.append(card);
+  }
+};
+
 const displayLessons = (lessons) => {
   // 1. Get the Container & empty
   const lessonContainer = document.getElementById("level-container");
@@ -14,7 +45,7 @@ const displayLessons = (lessons) => {
     // 3. Create element
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-                <button class="btn btn-outline btn-primary">
+                <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
                     <i class="fa-solid fa-book-open"></i>
                     Lesson-${lesson.level_no}
                 </button>
